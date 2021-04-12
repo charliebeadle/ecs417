f92037833ffc80a121a2e25ca1315531d0e8b055
Charlie Beadle <charliedebeadle@gmail.com>
2021-04-13 00:00:06 +0100

    conn

---
+++ webroot/assessment/config_local.php
@@ -0,0 +1,13 @@
+<?php
+  $servername = "127.0.0.1";
+  $username = "root";
+  $password = "";
+  $dbname = "ecs417";
+
+  GLOBAL $conn;
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+  }
+?>
